2fea2cfa78b89a3db22a718ec59e6e6802ea84a2
Meat Wood bert James <88901850+jameswalker2@users.noreply.github.com>
2023-11-15 22:27:57 -0500

    On move with design and search

--- src/components/base/Eleves.jsx
+++ src/components/base/Eleves.jsx
@@ -1,113 +1,144 @@
-import {NavLink} from "react-router-dom";
-import {BiArrowBack, BiSolidPencil} from 'react-icons/bi'
-import './Eleves.scss'
-import {FiSearch} from "react-icons/fi";
-import {FaRegTrashAlt} from "react-icons/fa";
-import {useEffect, useState} from "react";
-import {supabase} from "../login/SupabaseConfig.jsx";
-import {BsPersonFillAdd} from "react-icons/bs";
-
-
+import { NavLink } from "react-router-dom";
+import { BiArrowBack, BiSolidPencil } from "react-icons/bi";
+import "./Eleves.scss";
+import { FiSearch } from "react-icons/fi";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import { supabase } from "../login/SupabaseConfig.jsx";
+import { BsPersonFillAdd } from "react-icons/bs";
 
 export function Eleves() {
-    const [students, setStudents] = useState([])
-    const [search, setSearch] = useState('students')
+  // const [searchQuery, setSearchQuery] = useState("");
+  // const [searchResults, setSearchResults] = useState([]);
+  // const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const allStudents = async () => {
-            try {
-                const {data, error} = await supabase
-                    .from('students')
-                    .select('*')
-                    // .textSearch('search_students', 'id')
+  // const allStudents = async () => {
+  //   try {
+  //     const { data, error } = await supabase
+  //       .from("students")
+  //       .select("*")
+  //       .textSearch("firstName", searchQuery);
 
+  //     if (error) {
+  //       setError(error.message);
+  //     } else {
+  //       setSearchResults(data);
+  //     }
+  //   } catch (error) {
+  //     setError(error.message);
+  //   }
+  // };
 
-                if (data) {
-                    setStudents(data)
-                    setSearch(data)
-                    console.log(error)
-                }
-            } catch (error) {
-                console.log(error)
-            }
-        }
+  const [searchQuery, setSearchQuery] = useState("");
+  // const [searchResults, setSearchResults] = useState([]);
+  const [allResults, setAllResults] = useState([]);
+  const [error, setError] = useState(null);
 
-        allStudents()
-    }, [])
+  useEffect(() => {
+    fetchAllResults();
+  }, []);
 
-    // const handleDelete = async (id) => {
-    //     const value = doc(db, 'inscription', id)
-    //     await deleteDoc(value)
-    // }
-    console.log(search)
-    return (
-        <>
-                <NavLink to={'/accueil'}>
-                    <BiArrowBack  id="back"/>
-                </NavLink>
-            <div className="container_link_el">
-                <h1 id="container_h1">Eleves</h1>
-                <div>
-                    <NavLink className="link_el"  to={'/accueil'}>Dashboard </NavLink>
-                    <span>|</span>
-                    <NavLink className="link_el"  to={'/paiement'}>Paiement</NavLink>
-                </div>
-            </div>
-            <div className="container">
-                <div className="search_bar">
-                    <FiSearch id="icon_search"/>
-                    <input
-                        onChange={e => setSearch(e.target.value)}
-                        type="search" id="search_bar" placeholder="Cherchez un élève..."/>
-                    <NavLink id="button_add_el" to={'/inscription'}><BsPersonFillAdd id="btn_add_el"/> Ajouter</NavLink>
-                </div>
-                <h2 style={{color: 'white', margin: '20px 0'}} >Liste des élèves</h2>
-                <div>
-                    <table className="table_el">
-                        <thead key='thead'>
-                        <tr >
-                            <th>Nom</th>
-                            <th className="expand_el">Prénom</th>
-                            <th className="expand_el">Date de naissance</th>
-                            <th className="expand_el">Lieu de naissance</th>
-                            <th>Sexe</th>
-                            <th className="expand_el">classe</th>
-                            <th>Adresse</th>
-                            <th>Téléphone</th>
-                            <th>Actions</th>
-                        </tr>
-                        </thead>
-                        {students.map((student) => (
-                                <tbody key={student.id} className="scroll">
-                                <tr>
-                                    <td>{student.firstName}</td>
-                                    <td>{student.lastName}</td>
-                                    <td>{student.birth}</td>
-                                    <td>{student.adressBirth}</td>
-                                    <td>{student.gender}</td>
-                                    <td>{student.classe}</td>
-                                    <td>{student.adress}</td>
-                                    <td>{student.phone}</td>
-                                    <td>
-                                        <span className="actions">
-                                            <NavLink id="nav"
-                                                     // to={`/edit/${student.id}`
-                                                    to={`/edit/`
-                                            }>
-                                            <BiSolidPencil/>
-                                            </NavLink>
-                                            <FaRegTrashAlt
-                                              // onClick={() => handleDelete(student.id)}
-                                            />
-                                        </span>
-                                    </td>
-                                </tr>
-                                </tbody>
-                        ))}
-                    </table>
-                </div>
-            </div>
-        </>
-    )
+  const fetchAllResults = async () => {
+    try {
+      const { data, error } = await supabase
+        .from("students")
+        .select("*")
+        .textSearch(searchQuery);
+
+      if (error) {
+        setError(error.message);
+      } else {
+        setAllResults(data);
+        // setSearchQuery(data);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  console.log(allResults);
+  return (
+    <>
+      <NavLink to={"/accueil"}>
+        <BiArrowBack id="back" />
+      </NavLink>
+      <div className="container_link_el">
+        <h1 id="container_h1">Eleves</h1>
+        <div>
+          <NavLink className="link_el" to={"/accueil"}>
+            Dashboard{" "}
+          </NavLink>
+          <span>|</span>
+          <NavLink className="link_el" to={"/paiement"}>
+            Paiement
+          </NavLink>
+        </div>
+      </div>
+      <div className="container">
+        <div className="search_bar">
+          <FiSearch id="icon_search" />
+          <input
+            onChange={(e) => setSearchQuery(e.target.value)}
+            value={searchQuery}
+            type="search"
+            id="search_bar"
+            placeholder="Cherchez un élève..."
+          />
+          <NavLink id="button_add_el" to={"/inscription"}>
+            <BsPersonFillAdd id="btn_add_el" /> Ajouter
+          </NavLink>
+        </div>
+        <h2 style={{ color: "white", margin: "20px 0" }}>Liste des élèves</h2>
+        <div>
+          <table className="table_el">
+            <thead key="thead">
+              <tr>
+                <th>Nom</th>
+                <th className="expand_el">Prénom</th>
+                <th className="expand_el">Date de naissance</th>
+                <th className="expand_el">Lieu de naissance</th>
+                <th>Sexe</th>
+                <th className="expand_el">classe</th>
+                <th>Adresse</th>
+                <th>Téléphone</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            {allResults
+              .filter((result) =>
+                result.firstName
+                  .toLowerCase()
+                  .includes(searchQuery.toLowerCase())
+              )
+              .map((student) => (
+                <tbody key={student.id} className="scroll">
+                  <tr>
+                    <td>{student.firstName}</td>
+                    <td>{student.lastName}</td>
+                    <td>{student.birth}</td>
+                    <td>{student.adressBirth}</td>
+                    <td>{student.gender}</td>
+                    <td>{student.classe}</td>
+                    <td>{student.adress}</td>
+                    <td>{student.phone}</td>
+                    <td>
+                      <span className="actions">
+                        <NavLink
+                          id="nav"
+                          // to={`/edit/${student.id}`
+                          to={`/edit/`}>
+                          <BiSolidPencil />
+                        </NavLink>
+                        <FaRegTrashAlt
+                        // onClick={() => handleDelete(student.id)}
+                        />
+                      </span>
+                    </td>
+                  </tr>
+                </tbody>
+              ))}
+          </table>
+        </div>
+      </div>
+    </>
+  );
 }
-
--- src/components/pages/Dashboard/Dashboard.jsx
+++ src/components/pages/Dashboard/Dashboard.jsx
@@ -1,123 +1,140 @@
-import {useState, useEffect} from "react";
-import {NavBar} from "../../header/NavBar.jsx";
-import {NavLink} from "react-router-dom";
-import {DataIncome} from "../../accueil/DataIncome.jsx";
-import {DataExpense} from '../../accueil/DataExpense.jsx'
-import {ChartIncome} from "../../accueil/ChartIncome.jsx";
-import {ChartExpense} from '../../accueil/ChartExpense.jsx'
-import {motion} from "framer-motion";
-import {supabase} from '../../login/SupabaseConfig.jsx'
-import './Dashboard.scss'
- export function  Dashboard() {
+import { useState, useEffect } from "react";
+import { NavBar } from "../../header/NavBar.jsx";
+import { NavLink } from "react-router-dom";
+import { motion } from "framer-motion";
+import { supabase } from "../../login/SupabaseConfig.jsx";
+import Chart from "react-apexcharts";
+import "./Dashboard.scss";
 
-    const [text, setText] = useState([])
-
-     useEffect(() => {
-
-         async function numStudents () {
-             const {data, error} = await supabase
-                 .from('students')
-                 .select("*")
-
-             if(data){
-                 setText(data)
-             }
-             if (error) throw error
-         }
-
-          numStudents()
-     }, []);
-
-
-    const [dataIncome, setDataIncome] = useState({
-        labels: DataIncome.map((data) => data.week),
-        datasets: [{
-            label: "Rentrée",
-            data: DataIncome.map((data) => data.income),
-            backgroundColor: [
-                '#30038B',
-                '#00D1FF',
-                '#FF0000',
-                '#FFA901'
-            ]
+export function Dashboard() {
+  const [text, setText] = useState([]);
+  const [state, setState] = useState({
+    options: {
+      chart: {
+        id: "basic-bar",
+      },
+      xaxis: {
+        categories: ["Semaine 1", "Semaine 2", "Semaine 3", "Semaine 4"],
+        label: {
+          show: true,
         },
-        ],
-    });
+      },
+    },
+    series: [
+      {
+        name: "Revenu",
+        data: [1500, 2500, 500, 10000],
+        color: "#00ff00",
+      },
+      {
+        name: "Depense",
+        data: [20000, 200, 3000, 150],
+        color: "#ff2424",
+      },
+    ],
+  });
 
-    const [dataExpense, setDataExpense] = useState({
-        labels: DataExpense.map((data) => data.week),
-        datasets: [{
-            label: "Sortie",
-            data: DataExpense.map((data) => data.expense),
-            backgroundColor: [
-                '#30038B',
-                '#00D1FF',
-                '#FF0000',
-                '#FFA901'
-            ]
-        },
-        ],
-    });
-   // console.log(setDataIncome, setDataExpense)
+  useEffect(() => {
+    async function numStudents() {
+      const { data, error } = await supabase.from("students").select("*");
 
+      if (data) {
+        setText(data);
+      }
+      if (error) throw error;
+    }
 
-    return (
-        <>
-            <NavBar/>
-            <motion.div
-              initial={{opacity: 0, scaleY: 0}}
-              animate={{opacity: 1, scaleY: 1}}
-              exit={{opacity: 0, scaleY: 0}}
-              transition={{duration: 0.5, easeinout: [0.22, 1, 0.36, 1]}}>
-              <div className="container_dash">
-                <section className="dashboard">
-                  <h1>Dashboard</h1>
-                  <section className="content-dash">
-                    <NavLink  to={'/eleves'} id="ct">
-                        <div className="title">
-                            <h3>Elève</h3>
-                            <h2>{text.length}</h2>
-                        </div>
-                        <p>Total élèves</p>
-                    </NavLink>
-                    <NavLink to={'/staffs'} id="ct">
-                        <div className="title">
-                            <h3>Staff</h3>
-                            <h2>500</h2>
-                        </div>
+    numStudents();
+  }, []);
 
-                        <p>Total staffs</p>
-                    </NavLink>
-                    <NavLink to={'/profs'} id="ct">
-                        <div className="title">
-                            <h3>Prof</h3>
-                            <h2>100</h2>
-                        </div>
-                        <p>Total professeurs</p>
-                    </NavLink>
-                    <NavLink to={'/inscription'} id="ct">
-                        <div className="title">
-                            <h3>Incription</h3>
-                            <h2>10</h2>
-                        </div>
-                        <p>Total professeurs</p>
-                    </NavLink>
-                  </section>
-                </section>
-                <section className="revenu">
-                <h1 id="rev-h1">Total revenu du mois Octobre</h1>
-                <div id="revenu-bar">
-                <ChartIncome chartData={dataIncome}/>
+  return (
+    <>
+      <NavBar />
+      <motion.div
+        initial={{ opacity: 0, scaleY: 0 }}
+        animate={{ opacity: 1, scaleY: 1 }}
+        exit={{ opacity: 0, scaleY: 0 }}
+        transition={{ duration: 0.5, easeinout: [0.22, 1, 0.36, 1] }}>
+        {/*  */}
+        <div className="container_dash">
+          <section className="dashboard">
+            <h1>Dashboard</h1>
+            <p style={{ fontWeight: "200" }}>Bienvenue sur votre Dashboard</p>
+            {/*  */}
+            <section className="content-dash">
+              <NavLink to={"/eleves"} id="ct">
+                <div className="title">
+                  <h3>Elève</h3>
+                  <h2>{text.length}</h2>
                 </div>
-                </section>
-                <section className="depense">
-                <h1 id="dep-h1">Total depense du mois Octobre</h1>
-                <div id="depense-bar">
-                <ChartExpense chartData={dataExpense}/>
+                <p>Total élèves</p>
+              </NavLink>
+              <NavLink to={"/staffs"} id="ct">
+                <div className="title">
+                  <h3>Staff</h3>
+                  <h2>500</h2>
                 </div>
-                </section>
-              </div>
-            </motion.div>
-        </>
-    )
+                <p>Total staffs</p>
+              </NavLink>
+              <NavLink to={"/profs"} id="ct">
+                <div className="title">
+                  <h3>Prof</h3>
+                  <h2>100</h2>
+                </div>
+                <p>Total professeurs</p>
+              </NavLink>
+              <NavLink to={"/inscription"} id="ct">
+                <div className="title">
+                  <h3>Incription</h3>
+                  <h2>10</h2>
+                </div>
+                <p>Total professeurs</p>
+              </NavLink>
+            </section>
+          </section>
+          <section className="revenu">
+            <h4>Revenu et Depense</h4>
+            <h2>$20,156.30</h2>
+            <Chart
+              options={state.options}
+              series={state.series}
+              type="area"
+              width={"750"}
+              height={"320"}
+            />
+          </section>
+          <section className="recent">
+            <h4>Ajout récent</h4>
+            <table className="table_paiement">
+              <thead key="thead">
+                <tr>
+                  <th className="expand_bar">Nom Complet</th>
+                  <th className="expand_bar_2">Valeur Avancée</th>
+                  <th className="expand_bar_2">Balance</th>
+                </tr>
+              </thead>
+              <tbody className="scroll">
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td className="expand_bar"></td>
+                  <td>$2000</td>
+                  <td>$5000</td>
+                  <td>Versement 1</td>
+                  <td
+                    id="non"
+                    style={{
+                      fontWeight: "700",
+                      textTransform: "uppercase",
+                      color: "red",
+                    }}
+                  />
+                </tr>
+              </tbody>
+            </table>
+          </section>
+        </div>
+      </motion.div>
+    </>
+  );
 }
