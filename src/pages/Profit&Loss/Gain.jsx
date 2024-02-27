62124b8a1ecdda282d14b2e5b590373d430af210
Meat Wood bert James <88901850+jameswalker2@users.noreply.github.com>
2024-02-26 19:36:12 -0500

    On change the gain page

--- src/pages/Profit&Loss/Gain.jsx
+++ src/pages/Profit&Loss/Gain.jsx
@@ -1,5 +1,6 @@
-import { SearchOutlined } from "@ant-design/icons";
-import { DatePicker } from "antd";
+import { Button, DatePicker } from "antd";
+import { Search } from "lucide-react";
+import { NavLink } from "react-router-dom";
 import { NavBar } from "../../components/Navbar/NavBar";
 
 const { RangePicker } = DatePicker;
@@ -7,30 +8,60 @@ function Gain() {
   return (
     <>
       <NavBar />
-      <div className="h-screen px-52 py-10">
-        <div className="bg-white rounded-2xl w-[80rem] h-auto p-5">
-          <div className="text-[20px] font-medium text-color1 mb-5">
+      <div className="h-screen overflow-y-scroll pl-64 py-5 bg-primaryColor bg-opacity-10">
+        <div
+          className={
+            "text-sm breadcrumbs flex items-center justify-between w-[95%] h-16 p-4 " +
+            "text-supportingColor1 bg-white rounded-lg shadow-sm"
+          }>
+          <h1 className="font-semibold text-2xl">Staffs</h1>
+          <ul>
+            <li>
+              <NavLink className="text-supportingColor1" to={"/dashboard"}>
+                Dashboard
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="text-supportingColor1" to={"/caisse"}>
+                Caisse
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="text-supportingColor1" to={"/gain"}>
+                Profit & Perte
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+
+        <div className="w-[95%] p-4 rounded-lg bg-white mt-10 shadow-sm">
+          <div className="font-medium text-supportingColor1 mb-5">
             <h2>Sélectionnez les critères</h2>
           </div>
-          <div className="flex justify-center">
-            <RangePicker className="w-[40rem]" />
-            <button
+          <div className="flex flex-wrap justify-center">
+            <RangePicker
+              className="w-[40rem] border-primaryColor border-2 rounded-lg bg-white
+              focus:file-input-primaryColor"
+            />
+            <Button
               type="search"
-              className="btn bg-color2 text-white hover:bg-color1 ml-10 w-32">
-              <SearchOutlined />
+              className="btn btn-xs text-xs h-10 w-32 ml-20 border-none text-white bg-primaryColor
+              hover:bg-slate-100 hover:text-primaryColor active:bg-slate-100">
+              <Search size={20} />
               Search
-            </button>
+            </Button>
           </div>
         </div>
-        <div className="bg-white rounded-2xl w-[80rem] h-auto mt-20 p-5">
-          <div className="text-[20px] font-medium text-color1 mb-5">
+
+        <div className="w-[95%] p-4 rounded-lg bg-white mt-10 shadow-sm">
+          <div className="font-medium text-supportingColor1 mb-5">
             <h2>Profit et perte</h2>
           </div>
-          <div>
+          <div className="overflow-y-hidden overflow-x-auto h-auto mt-10 rounded-lg bg-white p-4">
             <table className="table table-xs">
               <thead
                 key="thead"
-                className="text-color2 text-sm bg-gray-50 hover:bg-gray-100">
+                className="text-supportingColor1 text-sm bg-primaryColor bg-opacity-10">
                 <tr>
                   <th>Date</th>
                   <th>Revenu</th>
@@ -39,14 +70,14 @@ function Gain() {
                   <th>Actions</th>
                 </tr>
               </thead>
-              <tbody key={"student.id"} className="text-2xl font-semibold">
+              <tbody key={"student.id"} className="font-semibold">
                 <tr>
                   <td>0</td>
                   <td>$ 20000</td>
                   <td>$ 1000</td>
                   <td>$ 1000</td>
                   <td>
-                    <button className="btn bg-color2 hover:bg-color1 text-white btn-xs">
+                    <button className="btn bg-color2 hover:bg-supportingColor1 text-white btn-xs">
                       Détails
                     </button>
                     <button className="btn bg-red-600 hover:bg-red-700 text-white btn-xs">
