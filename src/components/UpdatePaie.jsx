a94f7979379d82d02bce4bfd6ed095cd4980618a
Meat Wood bert James <88901850+jameswalker2@users.noreply.github.com>
2024-03-29 11:28:34 -0400

    Update the style on UpdatePaie.jsx

--- src/components/UpdatePaie.jsx
+++ src/components/UpdatePaie.jsx
@@ -1,6 +1,5 @@
-import { Modal } from "antd";
+import { Empty, Modal } from "antd";
 import { User } from "lucide-react";
-import moment from "moment";
 import { useEffect, useState } from "react";
 import { NavLink, useNavigate, useParams } from "react-router-dom";
 import { supabase } from "../Config/SupabaseConfig.jsx";
@@ -17,13 +16,14 @@ export function UpdatePaie() {
   const [date, setDate] = useState("");
   const [mode, setUpdateMode] = useState("");
   const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [classe, setClasse] = useState("");
   const [fatherName, setfatherName] = useState("");
   const [motherName, setmotherName] = useState("");
   const [linkPerson, setLinkPerson] = useState("");
   const [phone, setPhone] = useState("");
-  const [classe, setClasse] = useState("");
-  const [created_at, setCreated_at] = useState("");
-  const [lastName, setLastName] = useState("");
+  const [address, setAddress] = useState("");
+  const [email, setEmail] = useState("");
   const [nouveauMontant, setNouveauMontant] = useState(0);
   const [getId, setGetId] = useState("");
 
@@ -36,20 +36,21 @@ export function UpdatePaie() {
         .single();
 
       if (data) {
-        setName(data.firstName);
-        setfatherName(data.lastFather);
-        setmotherName(data.lastMother);
-        setLinkPerson(data.linkPerson);
-        setPhone(data.phone);
-        setClasse(data.classe);
+        setName(data.students.firstName);
+        setLastName(data.students.lastName);
+        setClasse(data.students.classe);
+        setfatherName(data.students.lastFather);
+        setmotherName(data.students.lastMother);
+        setLinkPerson(data.students.linkPerson);
+        setPhone(data.students.phone);
+        setAddress(data.students.address);
+        setEmail(data.students.email);
         setAmount(data.amount);
         setUpdateBalance(data.balance);
         setUpdateVersement(data.versement);
         setUpdateStatut(data.statut);
         setDate(data.date);
-        setCreated_at(data.created_at);
         setUpdateMode(data.mode);
-        setLastName(data.lastName);
         setGetId(data.student_id);
         console.log(data);
       } else {
@@ -170,155 +171,178 @@ export function UpdatePaie() {
               <User size={160} strokeWidth={1} />
             </div>
             <div className="">
-              <h2 className="font-medium text-xl">Nom Complet{name}</h2>
-              <h3 className="font-medium text-primaryColor">Classe{name}</h3>
+              <h2 className="font-medium text-xl">
+                {name} {lastName}
+              </h2>
+              <h3 className="font-medium text-primaryColor">{classe}</h3>
             </div>
           </div>
           <div className="ml-40">
-            <span className="flex">
-              <h2 className="font-medium mr-5">ID inscription :</h2>
+            <span className="flex justify-between mb-5">
+              <h2 className="font-semibold mr-40">ID inscription :</h2>
               <p>{id}</p>
             </span>
-            <span className="flex">
-              <h2 className="font-medium mr-5">Date inscription :</h2>
-              <p>{id}</p>
+            <span className="flex justify-between mb-5">
+              <h2 className="font-semibold mr-40">Nom du père :</h2>
+              <p>{fatherName}</p>
             </span>
-            <span className="flex">
-              <h2 className="font-medium mr-5">ID Inscription :</h2>
-              <p>{id}</p>
+            <span className="flex justify-between mb-5">
+              <h2 className="font-semibold mr-40">Nom de la mère :</h2>
+              <p>{motherName}</p>
             </span>
-            <span className="flex">
-              <h2 className="font-medium mr-5">ID Inscription :</h2>
-              <p>{id}</p>
+            <span className="flex justify-between mb-5">
+              <h2 className="font-semibold mr-40">
+                Nom personne responsable :
+              </h2>
+              <p>{linkPerson}</p>
+            </span>
+            <span className="flex justify-between mb-5">
+              <h2 className="font-semibold mr-40">Numéro téléphone :</h2>
+              <p className="">{phone}</p>
+            </span>
+            <span className="flex justify-between mb-5">
+              <h2 className="font-semibold mr-40">Adresse :</h2>
+              <p>{address}</p>
+            </span>
+            <span className="flex justify-between mb-5">
+              <h2 className="font-semibold mr-40">Email :</h2>
+              <p>{email}</p>
             </span>
           </div>
         </div>
 
         <div className="w-[95%] p-4 rounded-lg bg-white mt-10 shadow-sm">
-          <h2>Paiement info</h2>
-          <form onSubmit={handleUpdateFees}>
-            <div className="flex flex-wrap items-center p-10">
-              <label className="form-control w-full max-w-xs mr-5 mb-2">
-                <div className="label">
-                  <span className="label-text text-supportingColor1">
-                    Montant
-                  </span>
-                </div>
-                <input
-                  type="text"
-                  placeholder="Montant Avancé"
-                  value={nouveauMontant}
-                  onChange={handleNouveauMontantChange}
-                  className="input bg-slate-100 border-primaryColor border-2"
-                />
-                <div className="label">
-                  <span className="label-text-alt"></span>
-                </div>
-              </label>
+          {statut === "Payé" ? (
+            <Empty description={"Paiement déjà effectué"} />
+          ) : (
+            <div>
+              <h2>Paiement info</h2>
+              <form onSubmit={handleUpdateFees}>
+                <div className="flex flex-wrap items-center p-10">
+                  <label className="form-control w-full max-w-xs mr-5 mb-2">
+                    <div className="label">
+                      <span className="label-text text-supportingColor1">
+                        Montant
+                      </span>
+                    </div>
+                    <input
+                      type="text"
+                      placeholder="Montant Avancé"
+                      value={nouveauMontant}
+                      onChange={handleNouveauMontantChange}
+                      className="input bg-slate-100 border-primaryColor border-2"
+                    />
+                    <div className="label">
+                      <span className="label-text-alt"></span>
+                    </div>
+                  </label>
 
-              <label className="form-control w-full max-w-xs mr-5 mb-2">
-                <div className="label">
-                  <span className="label-text text-supportingColor1">
-                    Balance
-                  </span>
-                </div>
-                <input
-                  type="text"
-                  value={testAmount}
-                  placeholder="Balance"
-                  onChange={(e) => setUpdateBalance(e.target.value)}
-                  className="input bg-slate-100 border-primaryColor border-2"
-                />
-                <div className="label">
-                  <span className="label-text-alt"></span>
-                </div>
-              </label>
+                  <label className="form-control w-full max-w-xs mr-5 mb-2">
+                    <div className="label">
+                      <span className="label-text text-supportingColor1">
+                        Balance
+                      </span>
+                    </div>
+                    <input
+                      type="text"
+                      value={testAmount}
+                      placeholder="Balance"
+                      onChange={(e) => setUpdateBalance(e.target.value)}
+                      className="input bg-slate-100 border-primaryColor border-2"
+                    />
+                    <div className="label">
+                      <span className="label-text-alt"></span>
+                    </div>
+                  </label>
 
-              <label className="form-control w-full max-w-xs mr-5 mb-2">
-                <div className="label">
-                  <span className="label-text text-supportingColor1">
-                    Quel versement ?
-                  </span>
-                </div>
-                <select
-                  value={versement}
-                  className="select bg-slate-100 border-primaryColor border-2"
-                  onChange={(e) => setUpdateVersement(e.target.value)}>
-                  <option value="">Versement</option>
-                  <option value="Versement 1">Versement 1</option>
-                  <option value="Versement 2">Versement 2</option>
-                  <option value="Versement 3">Versement 3</option>
-                  <option value="Versement arierer">Versement arierer</option>
-                </select>
-                <div className="label">
-                  <span className="label-text-alt"></span>
-                </div>
-              </label>
+                  <label className="form-control w-full max-w-xs mr-5 mb-2">
+                    <div className="label">
+                      <span className="label-text text-supportingColor1">
+                        Quel versement ?
+                      </span>
+                    </div>
+                    <select
+                      value={versement}
+                      className="select bg-slate-100 border-primaryColor border-2"
+                      onChange={(e) => setUpdateVersement(e.target.value)}>
+                      <option value="">Versement</option>
+                      <option value="Versement 1">Versement 1</option>
+                      <option value="Versement 2">Versement 2</option>
+                      <option value="Versement 3">Versement 3</option>
+                      <option value="Versement arierer">
+                        Versement arierer
+                      </option>
+                    </select>
+                    <div className="label">
+                      <span className="label-text-alt"></span>
+                    </div>
+                  </label>
 
-              <label className="form-control w-full max-w-xs mr-5 mb-2">
-                <div className="label">
-                  <span className="label-text text-supportingColor1">
-                    Quel est le statut ?
-                  </span>
-                </div>
-                <select
-                  value={statut}
-                  onChange={(e) => setUpdateStatut(e.target.value)}
-                  className="select bg-slate-100 border-primaryColor border-2">
-                  <option value="">Statut</option>
-                  <option value="Non Payé">Non Payé</option>
-                  <option value="Avance">Avance</option>
-                  <option value="Payé">Payé</option>
-                </select>
-                <div className="label">
-                  <span className="label-text-alt"></span>
-                </div>
-              </label>
-              <label className="form-control w-full max-w-xs mr-5 mb-2">
-                <div className="label">
-                  <span className="label-text text-supportingColor1">
-                    Le type de paiement
-                  </span>
-                </div>
-                <select
-                  value={mode}
-                  onChange={(e) => setUpdateMode(e.target.value)}
-                  className="select bg-slate-100 border-primaryColor border-2">
-                  <option value="">Mode de paiement</option>
-                  <option value="Cash">Cash</option>
-                  <option value="Banque">Banque</option>
-                  <option value="Chèque">Chèque</option>
-                </select>
-                <div className="label">
-                  <span className="label-text-alt"></span>
-                </div>
-              </label>
-              <label className="form-control w-full max-w-xs mr-5 mb-2">
-                <div className="label">
-                  <span className="label-text text-supportingColor1">
-                    La date du paiement ?
-                  </span>
-                </div>
-                <input
-                  value={date}
-                  type="date"
-                  className="input bg-slate-100 border-primaryColor border-2"
-                  onChange={(e) => setDate(e.target.value)}
-                />
-                <div className="label">
-                  <span className="label-text-alt"></span>
-                </div>
-              </label>
+                  <label className="form-control w-full max-w-xs mr-5 mb-2">
+                    <div className="label">
+                      <span className="label-text text-supportingColor1">
+                        Quel est le statut ?
+                      </span>
+                    </div>
+                    <select
+                      value={statut}
+                      onChange={(e) => setUpdateStatut(e.target.value)}
+                      className="select bg-slate-100 border-primaryColor border-2">
+                      <option value="">Statut</option>
+                      <option value="Non Payé">Non Payé</option>
+                      <option value="Avance">Avance</option>
+                      <option value="Payé">Payé</option>
+                    </select>
+                    <div className="label">
+                      <span className="label-text-alt"></span>
+                    </div>
+                  </label>
+                  <label className="form-control w-full max-w-xs mr-5 mb-2">
+                    <div className="label">
+                      <span className="label-text text-supportingColor1">
+                        Le type de paiement
+                      </span>
+                    </div>
+                    <select
+                      value={mode}
+                      onChange={(e) => setUpdateMode(e.target.value)}
+                      className="select bg-slate-100 border-primaryColor border-2">
+                      <option value="">Mode de paiement</option>
+                      <option value="Cash">Cash</option>
+                      <option value="Banque">Banque</option>
+                      <option value="Chèque">Chèque</option>
+                    </select>
+                    <div className="label">
+                      <span className="label-text-alt"></span>
+                    </div>
+                  </label>
+                  <label className="form-control w-full max-w-xs mr-5 mb-2">
+                    <div className="label">
+                      <span className="label-text text-supportingColor1">
+                        La date du paiement ?
+                      </span>
+                    </div>
+                    <input
+                      value={date}
+                      type="date"
+                      className="input bg-slate-100 border-primaryColor border-2"
+                      onChange={(e) => setDate(e.target.value)}
+                    />
+                    <div className="label">
+                      <span className="label-text-alt"></span>
+                    </div>
+                  </label>
 
-              <button
-                className="btn bg-primaryColor text-white border-none 
+                  <button
+                    className="btn bg-primaryColor text-white border-none 
                 hover:bg-slate-100 hover:text-primaryColor font-normal"
-                type="submit">
-                Ajouter paiement
-              </button>
+                    type="submit">
+                    Ajouter paiement
+                  </button>
+                </div>
+              </form>
             </div>
-          </form>
-          {/*  */}
+          )}
           <h2>Résultat</h2>
           <table className="table">
             <thead key="thead" className="text-color1">
@@ -335,7 +359,6 @@ export function UpdatePaie() {
             <tbody key="f" className="scroll">
               <tr>
                 <td>{versement}</td>
-                <td>{moment(created_at).format("DD/MM/YYYY")}</td>
                 <td>{montantTotal}</td>
                 <td>{testAmount}</td>
                 <td>{date}</td>
